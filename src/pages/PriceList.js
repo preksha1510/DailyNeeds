7518ab14ffd39b2218dfcae1703cf032b70e4e3b
Preksha Rai B <preksharaib@Prekshas-MacBook-Pro.local>
2023-09-09 19:46:42 +0530

    Add. Button to add an item to the pricelist

--- src/pages/PriceList.js
+++ src/pages/PriceList.js
@@ -1,37 +1,61 @@
-import { render } from "enzyme";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AddItemsDialog from "./AddItemsDialog";
 
 function PriceList() {
-  const [itemList, setItemList] = useState([]);
+  const [data, setData] = useState([]);
+
+  const [openDialog, handleDisplay] = useState(false);
+
+  const handleClose = () => {
+    handleDisplay(false);
+  };
+
+  const openDialogBox = () => {
+    handleDisplay(true);
+  };
 
   useEffect(() => {
-    fetch("/priceList")
-      .then((response) => response.json())
-      .then((itemList) => setItemList(itemList));
-    console.log(itemList).catch((error) => console.error("Error:", error));
+    axios
+      .get("/priceList")
+      .then((response) => {
+        const priceList = response.data;
+        setData(priceList);
+      })
+      .catch((error) => console.error("Error:", error));
   }, []);
 
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Item Name</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {itemList.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.item_name}</td>
-              <td>{item.price}</td>
+    <>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Item Name</th>
+              <th>Price</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.itemName}</td>
+                <td>{item.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div>
+          <button data-testid="add-button" onClick={openDialogBox}>
+            Add
+          </button>
+          {openDialog && (
+            <AddItemsDialog open={openDialog} onClose={handleClose} />
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
